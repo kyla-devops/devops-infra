1ee3a56c9877bb613b46274f58659ceb03b5a213
K Ɏ L ∆ <kyla@KYLAs-MacBook-Pro.local>
2024-08-15 15:18:16 +0530

    AWS user creation

--- terraform-config/aws-instance/main.tf
+++ terraform-config/aws-instance/main.tf
@@ -1,5 +1,21 @@
-resource "aws_instance" "k8scluster" {
-    ami = var.ami
-    instance_type = var.instance_type
-    key_name = var.key
+resource "aws_iam_user" "devops-user" {
+    name = var.user[0]
+    tags = {
+        Description = var.user[1]
+    }
 }
+resource "aws_iam_policy" "devops-policy" {
+    name = "DevOps-User"
+    policy = file("devops-policy.json")
+}
+
+resource "aws_iam_user_policy_attachment" "devops-access" {
+    user = aws_iam_user.devops-user.name
+    policy_arn = aws_iam_policy.devops-policy.arn
+}
+
+# resource "aws_instance" "k8scluster" {
+#     ami = var.ami
+#     instance_type = "t2.micro"
+#     key_name = var.key
+# }
--- terraform-config/aws-instance/variables.tf
+++ terraform-config/aws-instance/variables.tf
@@ -1,5 +1,9 @@
 variable ami {
     type = string
-    default = ""
+    default = "ami-0ad21ae1d0696ad58"
     description = "EC2 instance in India region with Ubuntu"
+}
+variable user {
+    type = list(string)
+    default = ["devops-user","DevOps User for all implementation"]
 }
